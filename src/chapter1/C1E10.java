8909ff1584012a6b6b33b84ffce0e8988b647d61
vikash singh <singhvikash301094@gmail.com>
2017-12-11 14:55:08 +0530

    printing value of the unicode character in string

---
+++ src/chapter1/C1E10.java
@@ -0,0 +1,29 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class C1E10 {
+
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+
+        String line = in.nextLine();
+
+        int length = line.length();
+        int value;
+
+        //printing  the unicode character and its value
+        for(int i =0; i<length; i++)
+        {
+            value = (int)line.charAt(i);
+
+            if(value > 127)
+            {
+                System.out.println("unicode character is " + line.charAt(i) + " value is " + value);
+            }
+
+        }
+
+    }
+}
