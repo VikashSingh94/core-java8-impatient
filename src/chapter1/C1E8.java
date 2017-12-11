f8d66428fc31c356aa830c90f653e4adffb5f55a
vikash singh <singhvikash301094@gmail.com>
2017-12-11 13:59:54 +0530

    chapter 1 exercise 8

---
+++ src/chapter1/C1E8.java
@@ -0,0 +1,41 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class C1E8 {
+
+    private static void subString(String string) {
+        int length = string.length();
+
+        //Printing the non empty substring of string
+        //Time complexity O(N^2)
+        for (int i = 0; i < length; i++)
+        {
+            //empty string
+            String temp ="";
+
+            for(int k = i ;(k < length)  ; k++)
+            {
+                temp += Character.toString(string.charAt(k));
+                System.out.println(temp);
+            }
+
+
+        }
+
+    }
+
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        String string = in.nextLine();
+
+        if(!string.isEmpty())
+            subString(string);
+        else
+            System.out.println("empty string");
+
+    }
+
+
+}
