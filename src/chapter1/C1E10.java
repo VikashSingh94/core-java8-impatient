3e6737c4e4dc85c846ee14a9a302774aa7471408
vikash singh <singhvikash301094@gmail.com>
2017-12-11 14:59:55 +0530

    skip

--- src/chapter1/C1E10.java
+++ src/chapter1/C1E10.java
@@ -8,22 +8,5 @@ public class C1E10 {
     {
         Scanner in = new Scanner(System.in);
 
-        String line = in.nextLine();
-
-        int length = line.length();
-        int value;
-
-        //printing  the unicode character and its value
-        for(int i =0; i<length; i++)
-        {
-            value = (int)line.charAt(i);
-
-            if(value > 127)
-            {
-                System.out.println("unicode character is " + line.charAt(i) + " value is " + value);
-            }
-
-        }
-
     }
 }
