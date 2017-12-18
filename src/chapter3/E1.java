f5c48fc8ca66e7a31cde638acfef83aeb36f1e24
vikash singh <singhvikash301094@gmail.com>
2017-12-18 14:31:59 +0530

    Class implementing two interface having same method print

---
+++ src/chapter3/E1.java
@@ -0,0 +1,54 @@
+package chapter3;
+
+/**
+
+ Class implementing two interface having same method print
+
+ *
+ */
+
+
+interface  MyInterface
+{
+    double  PI = 3.14;
+
+    void print();
+
+}
+
+
+interface MyInterface2
+{
+    double PI = 3.15;
+
+    void print();
+}
+
+class MyClass implements MyInterface , MyInterface2
+{
+    @Override
+    public void print()
+    {
+        System.out.println(MyInterface.PI);
+    }
+
+//    @Override
+//    public void helloName(int i) {
+//        System.out.println("VS");
+//    }
+}
+
+
+public class E1 {
+
+
+
+    public static void main(String args[])
+    {
+        MyClass my = new MyClass();
+        my.print();
+
+
+    }
+
+}
