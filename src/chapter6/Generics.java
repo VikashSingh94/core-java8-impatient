750b1e35fa6e1987080db0fc7f1e4fb3b6e2c808
vikash singh <singhvikash301094@gmail.com>
2018-01-03 16:14:43 +0530

    Create a generics class and implements the comparator

---
+++ src/chapter6/Generics.java
@@ -0,0 +1,69 @@
+/**
+ *
+ * Create a generics class and implements the comparator
+ * @author Vikash Singh
+ * @Date   3 Jan 2018
+ *
+ */
+
+package chapter6;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class VikStructure<T>
+{
+    private T key;
+
+    public VikStructure( T key)
+    {
+        this.key = key;
+    }
+
+    public T getkey()
+    {
+        return  this.key;
+    }
+
+}
+
+class VikStructureComparator implements Comparator< VikStructure<Integer> >
+{
+    @Override
+    public int compare(VikStructure<Integer> obj1 ,VikStructure<Integer> obj2)
+    {
+        return  obj2.getkey() -  obj1.getkey();
+    }
+}
+
+
+class VikStructureComparatorString implements Comparator< VikStructure<String> >
+{
+    @Override
+    public int compare(VikStructure<String> obj1 ,VikStructure<String> obj2)
+    {
+        return  obj2.getkey().compareTo(obj1.getkey());
+    }
+}
+public class Generics {
+
+    public static void main(String args[])
+    {
+        VikStructure<Integer> vikStructure[ ] = new VikStructure[10];
+
+        for(int i = 0 ;i <10;i++)
+        {
+            vikStructure[i] = new VikStructure(i);
+        }
+
+       // Collections.sort(vikStructure, new VikStructureComparator());
+       Arrays.sort(vikStructure);
+
+        Arrays.stream(vikStructure).forEach(i -> System.out.println(i.getkey()));
+    }
+
+
+
+}
+
+
